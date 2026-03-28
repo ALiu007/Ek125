9029d8d1aba1d65a00b8efae876d7f421c1dbcb7
GetNugget <162396048+GetNugget@users.noreply.github.com>
2026-03-28 18:19:04 -0400

    Add files via upload

--- Module B.py
+++ Module B.py
@@ -1,16 +1,25 @@
 import time
-import random
 
+#Function to check answer
 def validation(correctAnswer):
     """Gets user input from the problem presented and returns answer validity and time"""
     answerValid = False
+    #Getting the time at the start of the input
     answerStart = time.time()
-    answer = int(input("Your Answer: "))
+    #Getting user input for their answer
+    answer = input("Your Answer: ")
+    #Confirming input is a int
+    while answer.isnumeric() == False:
+        print("Please Enter a Integer")
+        answer = input("Your Answer: ")
+
+    #Getting time at the end of the input and then subtracting the end from the start to get the seconds it took
     answerEnd = time.time()
     answerTime = answerEnd - answerStart
 
-    if answer == correctAnswer:
-        if answerTime < 1:
+    #Checks if the input answer equals the correct answer and then prints the points gained
+    if int(answer) == correctAnswer:
+        if answerTime < 2:
             print("Lightning Fast Answer! Time Bonus awarded (+3)")
         else:
             print("Correct Answer! (+1)")
@@ -18,6 +27,7 @@ def validation(correctAnswer):
     else:
         print("uh oh that's not right...")
 
+    #Returns answer validity as well as the answer time for statistics and point distribution
     return answerValid, answerTime
 
 
